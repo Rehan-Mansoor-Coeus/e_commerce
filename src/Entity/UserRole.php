7352c03af9dc4d4dc92aa872b03daec1a2ce6133
Rehan-Mansoor-Coeus <rehan.mansooor@coeus-solutions.de>
2022-01-31 16:41:12 +0500

    create admin by cmd

---
+++ src/Entity/UserRole.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRoleRepository::class)
+ */
+class UserRole
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Role::class)
+     */
+    private $Role;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getRole(): ?Role
+    {
+        return $this->Role;
+    }
+
+    public function setRole(?Role $Role): self
+    {
+        $this->Role = $Role;
+
+        return $this;
+    }
+}
